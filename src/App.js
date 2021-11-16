465aadaa8c1fefb691722256b59517b663d7af9c
smailovic <smaelcv@gmail.com>
2021-11-16 11:17:40 +0100

    markup with bootstrap done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,163 @@
-import React from "react";
-import "./style.css";
+import React, { Component } from 'react';
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <section className="bg-light">
+        <header className="">
+          <div className="bg-dark p-5 ">
+            <h1 className="text-warning text-center border-end border-start">
+              CV Maker
+            </h1>
+          </div>
+        </header>
+        <form className="container mt-2">
+          <section className="bg-light border border-light border-2 form-group">
+            <h1>Personal Information</h1>
+            <div className="">
+              <div className="m-2">
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="first name"
+                />
+              </div>
+              <div className="m-2">
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="last name"
+                />
+              </div>
+              <div className="m-2">
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="title"
+                />
+              </div>
+              <div className="m-2">
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="photo"
+                />
+              </div>
+              <div className="m-2">
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="Adress"
+                />
+              </div>
+
+              <div className="m-2">
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="Phone number"
+                />
+              </div>
+              <div className="m-2">
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="m-2">
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="Description"
+                />
+              </div>
+            </div>
+
+            <h1>Education</h1>
+            <div className="m-2">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="University name"
+              />
+            </div>
+            <div className="m-2">
+              <input className="form-control" type="text" placeholder="city" />
+            </div>
+            <div className="m-2">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Degree"
+              />
+            </div>
+            <div className="m-2">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="subject"
+              />
+            </div>
+            <div className="m-2">
+              <input className="form-control" type="text" placeholder="from" />
+            </div>
+            <div className="m-2">
+              <input className="form-control" type="text" placeholder="to" />
+            </div>
+            <div className="m-2 bg-dark text-light text-center p-2 mb-2 rounded">
+              delete
+            </div>
+            <div className="m-2 bg-dark text-light text-center p-2 rounded">
+              Add
+            </div>
+
+            <h1>Experience</h1>
+            <div className="m-2">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Postition"
+              />
+            </div>
+            <div className="m-2">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Company"
+              />
+            </div>
+            <div className="m-2">
+              <input className="form-control" type="text" placeholder="City" />
+            </div>
+            <div className="m-2">
+              <input className="form-control" type="text" placeholder="From" />
+            </div>
+            <div className="m-2">
+              <input className="form-control" type="text" placeholder="To" />
+            </div>
+            <div className="bg-dark text-light text-center p-2 m-2 rounded">
+              delete
+            </div>
+            <div className="bg-dark text-light text-center p-2 m-2 rounded">
+              Add
+            </div>
+            <div className="bg-success text-center p-2 m-2 rounded">
+              Generate PDF
+            </div>
+            <div className="bg-danger text-center p-2 m-2 rounded">
+              Load Example
+            </div>
+            <div className="bg-info text-center p-2 m-2 rounded">Reset</div>
+          </section>
+        </form>
+      </section>
+    );
+  }
 }
+export default App;
